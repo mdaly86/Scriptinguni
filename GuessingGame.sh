be2c90beb4a648859be26939392f93080c82cc3d
mdaly86 <65539825+mdaly86@users.noreply.github.com>
2020-06-07 01:42:23 +0000

    Add Week 4 scripts to portfolio

---
+++ GuessingGame.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+n1=42
+guesses=1
+echo -n "Please type a number between 1 and 100: Your guess:"
+
+while read n2; do
+
+if   [[ $n2 -eq $n1 ]]; then
+break;  
+else
+echo    
+if [[ $n2 -gt $n1 ]]; then 
+echo -n "Sorry, your guess is too high. New guess:"
+elif [[ $n2 -lt $n1 ]]; then
+echo -n "Sorry, your guess is too low. New guess:"
+fi      
+fi
+guesses=$((guesses+1))
+
+done
+echo
+echo "Good job! It took you $guesses guesses to get the right number."
---
+++ IpAddressesOnly.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#get info about networking from the ifconfig command
+net_info="$(ifconfig)"
+#parse out the ip address lines using sed
+addresses=$(echo "$net_info" | sed -n '/inet / {
+s/inet/IP Address:/
+p
+}')
+#format output
+echo -e "IP addresses on this computer are:\n$addresses"
