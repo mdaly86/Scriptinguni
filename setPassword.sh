b6312c646555d9a4f5f2e0b2bafe781b5f789b5c
mdaly86 <65539825+mdaly86@users.noreply.github.com>
2020-06-20 12:58:37 +1000

    Delete setPassword.sh

--- setPassword.sh
+++
@@ -1,8 +0,0 @@
- #! /bin/bash
-
-read -p 'Foldername  : ' foldername_var
-echo "Foldername : $foldername_var"
-read -sp 'Password : ' pass_var
-echo Password Changed 
-echo "$pass_var" | sha256sum  > secret.txt
-
